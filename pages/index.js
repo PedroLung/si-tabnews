44875c6a268405db22ea3841fe8159a3fd35338d
PedroLung <pguedeslsilva@gmail.com>
2026-05-22 12:07:13 -0300

    concluir o desafio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 
 function Home() {
-    return <h1>Lis, minha princesa, EU TE AMOOOOOOOOO MUITÃOOOOO e se você me ama também me dá um beijão!❤️</h1>
+    return (
+        <div>
+            <h1>Olha amor dediquei esse pedaço de Internet só pra você!</h1>
+            <p>Minha princesa, EU TE AMOOOOOOOOO MUITÃOOOOO e se você me ama também me dá um beijão!❤️</p>
+            <br />
+            <br />
+            <p><strong>Ps:</strong> Caso queira acompanhar a minha saga, eu vou sempre atualizar esse site aqui! Quando quiser só conferir gatinha!</p>
+        </div>
+    )
 }
 
 export default Home;
