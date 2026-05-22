6f93ca7fbf007bd43be7cfc1ac2599a37b4d0e0e
PedroLung <pguedeslsilva@gmail.com>
2026-05-22 11:31:28 -0300

    corrigir a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 function Home() {
-    return <h1>Lis, minha princesa, EU TE AMOOOOOOOOO MUITÃOOOOO e se me ama também me dá um beijão!❤️</h1>
+    return <h1>Lis, minha princesa, EU TE AMOOOOOOOOO MUITÃOOOOO e se você me ama também me dá um beijão!❤️</h1>
 }
 
 export default Home;
