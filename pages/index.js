f8d0d29d79a35f0b5351ca1d72af6116b778e894
PedroLung <pguedeslsilva@gmail.com>
2026-05-22 11:07:19 -0300

    modificar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 function Home() {
-    return <h1>Lis, meu amor, EU TE AMOOOOOOOOO MUITÃOOOOO e se me ama também me dá um beijão</h1>
+    return <h1>Lis, minha princesa, EU TE AMOOOOOOOOO MUITÃOOOOO e se me ama também me dá um beijão!❤️</h1>
 }
 
 export default Home;
